423ab0f46ca3f284e70a05f7096f8c215e2c654c
Joshua Jungen <joshua.jungen@outlook.de>
2017-04-04 09:13:19 +0200

    Implemented Buy-Function

--- app/src/main/java/adp/group10/roomates/ShoppingListAdapter.java
+++ app/src/main/java/adp/group10/roomates/ShoppingListAdapter.java
@@ -14,6 +14,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 import adp.group10.roomates.activities.AddEditItemActivity;
@@ -34,16 +36,19 @@ public class ShoppingListAdapter extends ArrayAdapter<ShoppingListEntry> {
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-        if (view == null){
-            view = LayoutInflater.from(getContext()).inflate(R.layout.shopping_list_entry_data_item, parent, false);
+        if (view == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.shopping_list_entry_data_item,
+                    parent, false);
         }
 
         TextView tvName = (TextView) view.findViewById(R.id.entryName);
         TextView tvAmount = (TextView) view.findViewById(R.id.entryAmount);
-        
+        TextView tvPrice = (TextView) view.findViewById(R.id.entryPrice);
+
         final ShoppingListEntry entry = getItem(position);
         tvName.setText(entry.getName());
         tvAmount.setText("" + entry.getAmount());
+        tvPrice.setText("" + entry.getPrice());
 
 
         Button editButton = (Button) view.findViewById(R.id.editButton);
@@ -53,29 +58,45 @@ public class ShoppingListAdapter extends ArrayAdapter<ShoppingListEntry> {
                 Intent intent = new Intent(getContext(), AddEditItemActivity.class);
                 Bundle extra = new Bundle();
                 extra.putSerializable(ShoppingListStorage.class.getSimpleName(), entry);
+                extra.putString(AddEditItemActivity.MODE, AddEditItemActivity.MODE_EDIT);
                 intent.putExtras(extra);
 
-                ((Activity)getContext()).startActivityForResult(intent, ShoppingListActivity.EDIT_SHOPPING_LIST_ENTRY);
+                ((Activity) getContext()).startActivityForResult(intent,
+                        ShoppingListActivity.EDIT_SHOPPING_LIST_ENTRY);
                 notifyDataSetChanged();
             }
         });
 
         Button deleteButton = (Button) view.findViewById(R.id.deleteButton);
-                deleteButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ShoppingListStorage.getInstance().deleteEntry(entry);
-                        notifyDataSetChanged();
-                    }
-                });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShoppingListStorage.getInstance().deleteEntry(entry);
+                notifyDataSetChanged();
+            }
+        });
 
-        /* TODO Implement Block and Buy listeners
+        /* TODO Implement Block listener
         Button blockButton = (Button) view.findViewById(R.id.blockButton);
         blockButton.setOnClickListener(blockButtonClickHandler);
+        */
 
         Button buyButton = (Button) view.findViewById(R.id.buyButton);
-        buyButton.setOnClickListener(buyButtonClickHandler);
-        */
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AddEditItemActivity.class);
+                Bundle extra = new Bundle();
+                extra.putSerializable(ShoppingListStorage.class.getSimpleName(), entry);
+                extra.putString(AddEditItemActivity.MODE, AddEditItemActivity.MODE_BUY);
+                intent.putExtras(extra);
+
+                ((Activity) getContext()).startActivityForResult(intent,
+                        ShoppingListActivity.EDIT_SHOPPING_LIST_ENTRY);
+                notifyDataSetChanged();
+            }
+        });
+
         return view;
     }
 
