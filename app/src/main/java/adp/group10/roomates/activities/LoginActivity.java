63716c9e636d139826864f2f2cc33307dcd582c7
rim shahin <reemshahin@gmail.com>
2017-04-24 13:36:13 +0200

    user password validation

--- app/src/main/java/adp/group10/roomates/activities/LoginActivity.java
+++ app/src/main/java/adp/group10/roomates/activities/LoginActivity.java
@@ -6,15 +6,28 @@ import android.widget.Button;
 import android.content.Intent;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.Toast;
+import android.content.Context;
 
-//import com.google.firebase.database.ValueEventListener;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import adp.group10.roomates.R;
+import adp.group10.roomates.backend.model.ShoppingListEntry;
 
+import adp.group10.roomates.businesslogic.LoginManager;
 public class LoginActivity extends AppCompatActivity {
 
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -22,8 +35,8 @@ public class LoginActivity extends AppCompatActivity {
         Button bUserLogin = (Button) findViewById(R.id.bLogin);
         bUserLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                EditText etUsername;
-                EditText etPassword;
+                final EditText etUsername;
+                final EditText etPassword;
                 etUsername   = (EditText)findViewById(R.id.etUserName);
                 etPassword  = (EditText)findViewById(R.id.etPassword);
                 if (etUsername.getText().toString().length() ==0 )
@@ -34,8 +47,47 @@ public class LoginActivity extends AppCompatActivity {
                      etPassword.requestFocus();  }
                 else
                 {
-                    //DatabaseReference mTemplateRef= FirebaseDatabase.getInstance().getReference().child("users");
-                    startActivity(new Intent(LoginActivity.this, OptionsActivity.class));
+
+
+                    DatabaseReference mTemplateRef= FirebaseDatabase.getInstance().getReference("users");
+                    mTemplateRef.addListenerForSingleValueEvent(new ValueEventListener(){
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    String password = dataSnapshot.child(etUsername.getText().toString()).child("password").getValue(String.class);
+                    //String user = dataSnapshot.child(etUsername.getText().toString()).getValue(String.class);
+
+
+
+                        if ( !password.equals( etPassword.getText().toString() )   )
+                         {   etUsername.setError("Wrong User name or Password");
+                             etUsername.requestFocus(); }
+                        else
+                        { startActivity(new Intent(LoginActivity.this, OptionsActivity.class));}
+
+                        /*LoginManager Loginmanager= new LoginManager();
+                        Loginmanager.CheckUserPassword(etUsername.getText().toString(), etPassword.getText().toString());
+                        Context context = getApplicationContext();
+                        CharSequence text = Loginmanager.Pass;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();*/
+
+                        /**Context context = getApplicationContext();
+                        CharSequence text = etPassword.getText().toString() ;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();*/
+                }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                  }
+                });
+
 
                 }
 
@@ -65,6 +117,8 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onStart();
     }
+
+
 
 
 
