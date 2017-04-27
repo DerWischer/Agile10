78d27f73ea315dea37e1fa6e8d6af49a1f2171a9
rim shahin <reemshahin@gmail.com>
2017-04-27 15:18:14 +0200

    after creating a user to go to a group selection activity.

--- app/src/main/java/adp/group10/roomates/activities/RegisterActivity.java
+++ app/src/main/java/adp/group10/roomates/activities/RegisterActivity.java
@@ -110,7 +110,7 @@ public class RegisterActivity extends AppCompatActivity {
                     //startActivity(new Intent(RegisterActivity.this, ShoppingListActivity.class))
                     LoginActivity.currentuser = etUsername.getText().toString().trim();
                     finish();
-                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                    startActivity(new Intent(RegisterActivity.this, SelectGroupActivity.class));
                 }
 
             }
