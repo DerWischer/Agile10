e3b1aa2c1bbb74a40b4b69fc62de07dd5338381b
Fabian Stolz <fabian_stolz@hotmail.de>
2017-05-02 18:12:12 +0200

    Shopping List Items now have + and - buttons

--- app/src/main/java/adp/group10/roomates/businesslogic/ShoppingListFBAdapter.java
+++ app/src/main/java/adp/group10/roomates/businesslogic/ShoppingListFBAdapter.java
@@ -1,21 +1,26 @@
 package adp.group10.roomates.businesslogic;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.Query;
 
 import adp.group10.roomates.R;
+import adp.group10.roomates.activities.MainActivity;
 import adp.group10.roomates.backend.model.ShoppingListEntry;
+import adp.group10.roomates.fragments.ShoppingListFragment;
 
 /**
  * Created by Joshua Jungen on 22.04.2017.
  */
 
-public class ShoppingListFBAdapter extends FirebaseListAdapter<ShoppingListEntry> {
+public class ShoppingListFBAdapter extends FirebaseListAdapter<ShoppingListEntry>  {
+
+    private MainActivity mainActivity;
+    private ShoppingListFragment shoppingListFragment;
 
     /**
      * @param activity The activity containing the ListView
@@ -24,19 +29,21 @@ public class ShoppingListFBAdapter extends FirebaseListAdapter<ShoppingListEntry
      *                 using some combination of {@code limit()}, {@code startAt()}, and {@code
      *                 endAt()}.
      */
-    public ShoppingListFBAdapter(Activity activity, Query ref) {
+    public ShoppingListFBAdapter(Activity activity, Query ref, ShoppingListFragment fragment) {
         super(activity, ShoppingListEntry.class, R.layout.shopping_list_entry_data_item, ref);
+//        this.mainActivity = (MainActivity) activity;
+        this.shoppingListFragment = fragment;
     }
 
     @Override
     protected void populateView(View view, ShoppingListEntry model, final int position) {
         TextView tvName = (TextView) view.findViewById(R.id.etName);
-        TextView tvAmount = (TextView) view.findViewById(R.id.etAmount);
+        TextView etAmount = (TextView) view.findViewById(R.id.etAmount);
         TextView tvBlockedBy = (TextView) view.findViewById(R.id.etBlockedBy);
 
         ShoppingListEntry entry = getItem(position);
         tvName.setText(entry.getName());
-        tvAmount.setText("" + entry.getAmount());
+        etAmount.setText("" + entry.getAmount());
         if (entry.isBlocked()){
             tvBlockedBy.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_lock_outline_black_24dp, 0, 0, 0);
             tvBlockedBy.setText(entry.getBlockedBy());
@@ -46,5 +53,33 @@ public class ShoppingListFBAdapter extends FirebaseListAdapter<ShoppingListEntry
 
         }
 
+    }
+
+/*    public void setItemClickListener(View.OnClickListener ocl) {
+        int count = getCount();
+        for (int i = 0; i < count; i++) {
+            ViewGroup vg = (ViewGroup) getView(i, null, null);
+            vg.findViewById(R.id.etAmount).setOnClickListener(ocl);
+            vg.findViewById(R.id.etAmountPlus).setOnClickListener(ocl);
+            vg.findViewById(R.id.etAmountMinus).setOnClickListener(ocl);
+        }
+    }*/
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewGroup viewGroup = (ViewGroup) super.getView(position, convertView, parent);
+        View etAmount = viewGroup.findViewById(R.id.etAmount);
+        View etPlus = viewGroup.findViewById(R.id.etAmountPlus);
+        View etMinus = viewGroup.findViewById(R.id.etAmountMinus);
+
+        etAmount.setTag(position);
+        etPlus.setTag(position);
+        etMinus.setTag(position);
+
+        etAmount.setOnClickListener(shoppingListFragment);
+        etPlus.setOnClickListener(shoppingListFragment);
+        etMinus.setOnClickListener(shoppingListFragment);
+
+        return viewGroup;
     }
 }
--- app/src/main/java/adp/group10/roomates/fragments/ShoppingListFragment.java
+++ app/src/main/java/adp/group10/roomates/fragments/ShoppingListFragment.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,8 +17,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DatabaseReference;
@@ -45,7 +47,7 @@ import adp.group10.roomates.businesslogic.ShoppingListFBAdapter;
  */
 public class ShoppingListFragment extends Fragment implements AbsListView.MultiChoiceModeListener,
         AddItemsFragment.OnFragmentInterActionListener,
-        AdapterView.OnItemClickListener {
+        AdapterView.OnItemClickListener, View.OnClickListener {
 
     private OnFragmentInteractionListener mListener;
 
@@ -73,11 +75,14 @@ public class ShoppingListFragment extends Fragment implements AbsListView.MultiC
 
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference(
                 FirebaseHandler.KEY_SHOPPING_LIST + "/" + LoginActivity.currentGroup);
-        fbAdapter = new ShoppingListFBAdapter(getActivity(), ref);
+        fbAdapter = new ShoppingListFBAdapter(getActivity(), ref, this);
         gvList.setAdapter(fbAdapter);
         gvList.setOnItemClickListener(this);
         gvList.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
         gvList.setMultiChoiceModeListener(this);
+
+
+//        fbAdapter.setItemClickListener(this);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -102,6 +107,17 @@ public class ShoppingListFragment extends Fragment implements AbsListView.MultiC
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int position = (int) v.getTag();
+        MainActivity currentActivity = (MainActivity) getActivity();
+        if (v.getId() == R.id.etAmountPlus) {
+            currentActivity.incrementShoppingCartItem(fbAdapter.getItem(position).getName(), +1);
+        } else if (v.getId() == R.id.etAmountMinus) {
+            currentActivity.incrementShoppingCartItem(fbAdapter.getItem(position).getName(), -1);
+        }
     }
 
     /**
@@ -217,7 +233,7 @@ public class ShoppingListFragment extends Fragment implements AbsListView.MultiC
             LayoutInflater inflater = getActivity().getLayoutInflater();
             final View dialogView = inflater.inflate(R.layout.dialog_edit_item, null);
             final EditText etName = (EditText) dialogView.findViewById(R.id.etName);
-            final EditText etAmount = (EditText) dialogView.findViewById(R.id.etAmount);
+            final TextView etAmount = (TextView) dialogView.findViewById(R.id.etAmount);
             etName.setText(entry.getName());
             etAmount.setText("" + entry.getAmount());
 
@@ -261,7 +277,7 @@ public class ShoppingListFragment extends Fragment implements AbsListView.MultiC
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                EditText etAmount = (EditText) dialogView.findViewById(R.id.etAmount);
+                TextView etAmount = (TextView) dialogView.findViewById(R.id.etAmount);
                 String strPrice = etAmount.getText().toString().trim();
                 double price = Double.parseDouble(strPrice);
                 DatabaseReference transactionReference =
@@ -301,8 +317,10 @@ public class ShoppingListFragment extends Fragment implements AbsListView.MultiC
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        Toast toast = Toast.makeText(getActivity(), ""+view.getId(), Toast.LENGTH_SHORT);
+//        toast.show();
         MainActivity currentActivity = (MainActivity) getActivity();
-        currentActivity.incrementShoppingCartItem(fbAdapter.getItem(position).getName(), -1);
+//        currentActivity.incrementShoppingCartItem(fbAdapter.getItem(position).getName(), -1);
         // TODO Decrease amount, if 0 remove item
     }
 
